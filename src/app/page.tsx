1c372e788e961280df056865523f63a285ba779f
Christopher Dabu <christopher.dabu@pulsar-sd.com>
2025-08-28 22:37:28 +0800

    Refactor homepage cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,37 @@
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+type Blurb = {
+  key: number;
+  title: string;
+  content: string;
+};
 
+
+export default function Home() {
+  const blurbCard:Blurb[] = [
+    {
+      key: 1,
+      title: "Improve workflow",
+      content: "Technology is a force multiplier. I enjoy building tools intended to be used in day-to-day operations of small and medium businesses that make your operations more productive."
+    },
+    {
+      key: 2,
+      title: "Streamline information",
+      content: "Collection, categorization, and visualization of raw data is fulfilling for me. The software I build intends to give you the building blocks to make informed decisions."
+    },
+    {
+      key: 3,
+      title: "Improve workflow",
+      content: "I will work with what you have to the best of my ability. Utilizing open-source applications, we can build something together that will pay for itself before you know it."
+    },
+  ]
 
   return (
     <main className="content">
       <div className="hero-section">
         <div className="hero-content flex flex-col items-center md:items-start">
-          <Image src="/mmmaske.svg" width={300} height={150} alt="mmmaske spash image" />
+          <h2 className="text-4xl font-bold mb-4">mmmaske</h2>
           <p>Professional Developer. Amateur Systems Administrator. All-around Computer Dude.</p>
         </div>
       </div>
@@ -37,18 +60,12 @@ export default function Home() {
         <section className="mb-12">
           <h2 className="text-3xl font-bold mb-4">What I Do</h2>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-            <div className="bg-gray-800 p-6 rounded-lg shadow-md">
-              <h3 className="text-xl font-semibold mb-2">Improve workflow</h3>
-              <p className="text-gray-400">Technology is a force multiplier. I enjoy building tools intended to be used in day-to-day operations of small and medium businesses that make your operations more productive. </p>
-            </div>
-            <div className="bg-gray-800 p-6 rounded-lg shadow-md">
-              <h3 className="text-xl font-semibold mb-2">Streamline information</h3>
-              <p className="text-gray-400">Collection, categorization, and visualization of raw data is fulfilling for me. The software I build intends to give you the building blocks to make informed decisions. </p>
-            </div>
-            <div className="bg-gray-800 p-6 rounded-lg shadow-md">
-              <h3 className="text-xl font-semibold mb-2">Integrating technology</h3>
-              <p className="text-gray-400">I will work with what you have to the best of my ability. Utilizing open-source applications, we can build something together that will pay for itself before you know it!</p>
-            </div>
+            {blurbCard.map(blurb => (
+              <div className="bg-red-950/50 p-6 rounded-lg shadow-md">
+                <h3 className="text-xl font-semibold mb-2">{blurb.title}</h3>
+                <p className="text-gray-400">{blurb.content}</p>
+              </div>
+            ))}
           </div>
         </section>
       </div>
